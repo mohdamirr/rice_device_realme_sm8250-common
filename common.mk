6a1ae3cd1a6eb1cb3cad9ea8799c6422b039819a
MOHD AMIR ALI <78249996+mohdamirr@users.noreply.github.com>
2023-02-23 23:08:08 +0530

    sm8250-common:Drop Gcamgo
    * aperture cam already in ricedroid

--- common.mk
+++ common.mk
@@ -124,9 +124,6 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libstdc++.vendor
 
-PRODUCT_PACKAGES += \
-    Camera2 \
-    GcamGo
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
